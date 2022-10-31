622d655c4b63b2489d27e3a4c4864d8c1cc26450
Brandon <70345941@continental.edu.pe>
2022-10-31 15:59:19 -0500

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+// No funciona así. Tenemos q importar el archivo con la extensión js
+// const data = require('./data.json');
+
+// const body = document.getElementsByTagName('body')[0];
+
+// body.textContent = JSON.stringify(data);
